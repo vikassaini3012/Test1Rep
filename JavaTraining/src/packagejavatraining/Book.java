aa71447a49d7153407633263fc53adc0a1e7f6de
VIKASSAI <VIKASSAI@VIKASSAI03.corp.amdocs.com>
2018-10-02 15:27:35 +0530

    vikas push

--- JavaTraining/src/packagejavatraining/Book.java
+++ JavaTraining/src/packagejavatraining/Book.java
@@ -10,7 +10,7 @@ public class Book
 		irem = 10 % 3;
 		dresult = 10.0 / 3.0;
 		drem = 10.0 % 3.0;
-		System.out.println("RRResult and remainder of 10 / 3: " +
+		System.out.println("ReResult and remainder of 10 / 3: " +
 		iresult + " " + irem);
 		System.out.println("Result and remainder of 10.0 / 3.0: "
 		+ dresult + " " + drem);
